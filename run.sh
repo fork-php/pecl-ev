ff440d3864736634312ea7a1e794e7383ef04a96
Ruslan Osmanov <rrosmanov@gmail.com>
2021-07-18 09:14:58 +0300

    Internal: fixed shell scripts to run tests when sockets ext. is compiled as a shared lib.

--- run.sh
+++ run.sh
@@ -1,2 +1,13 @@
 #!/bin/bash -
-php -n -d extension=ev.so  -dextension_dir=./.libs "$@"
+sockets_so_path="$(php-config --extension-dir)/sockets.so"
+sockets_option=
+if [ -e "$sockets_so_path" ]; then
+  cp "$sockets_so_path" ./.libs/
+  sockets_option='-dextension=sockets.so'
+fi
+
+MALLOC_PERTURB_=$(($RANDOM % 255 + 1)) \
+	MALLOC_CHECK_=3 \
+	USE_ZEND_ALLOC=0 \
+	ZEND_DONT_UNLOAD_MODULES=1 \
+	php -n $sockets_option -dextension=ev.so  -dextension_dir=./.libs "$@"
